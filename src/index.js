3c53d87cf80f1b96d2088b02869ed4885afa1e5c
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2025-01-09 21:29:43 -0300

    feat: adiciona nodemon

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT
 
+app.get('/teste/leozito', (req, res) => {
+    res.send('Boa noite famíslia')
+})
+
 app.listen(port, () => {
     console.log(`Servidor iniciado na port ${port}`)
 })
